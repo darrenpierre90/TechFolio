c28a7053b37b9beeb09d666d35ee717855a4e240
Darren <d-pierre90@hotmail.com>
2020-07-23 15:33:47 -0400

    Updates

---
+++ precache-manifest.f316773bf2b8ef0e8c976e5783bf8a5b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21decbc0cefefbebc68d1b568e1ef102",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c964ecd69f155e279c6c",
+    "url": "/static/css/2.ffdfee16.chunk.css"
+  },
+  {
+    "revision": "1985eaedc927f8d72439",
+    "url": "/static/css/4.bbaea54d.chunk.css"
+  },
+  {
+    "revision": "0995151478d5dc23e4b6",
+    "url": "/static/css/main.46399a82.chunk.css"
+  },
+  {
+    "revision": "c964ecd69f155e279c6c",
+    "url": "/static/js/2.23a9e5ce.chunk.js"
+  },
+  {
+    "revision": "573c03b54bf739ab0b9029188d0efb17",
+    "url": "/static/js/2.23a9e5ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5fe30d0cf8c603083b5",
+    "url": "/static/js/3.9e35701a.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/3.9e35701a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1985eaedc927f8d72439",
+    "url": "/static/js/4.9eeb8c87.chunk.js"
+  },
+  {
+    "revision": "0995151478d5dc23e4b6",
+    "url": "/static/js/main.997026ee.chunk.js"
+  },
+  {
+    "revision": "d6e927de5fee58c51dbe",
+    "url": "/static/js/runtime-main.d4f7f54f.js"
+  },
+  {
+    "revision": "3e7b4ed42120fe2c30460e8ad09f7070",
+    "url": "/static/media/avatar.3e7b4ed4.jpeg"
+  },
+  {
+    "revision": "41863aa09de215b05408557053972480",
+    "url": "/static/media/avatar.41863aa0.JPG"
+  },
+  {
+    "revision": "f971dd94f62c4662e732ea74d2561260",
+    "url": "/static/media/ayin_qoph.f971dd94.png"
+  },
+  {
+    "revision": "aea271b930d57b95ca626bb1a9fb27ee",
+    "url": "/static/media/contact.aea271b9.svg"
+  },
+  {
+    "revision": "075fe2c07825b280a4a6c902e7e06631",
+    "url": "/static/media/defaultAvatar.075fe2c0.svg"
+  },
+  {
+    "revision": "1976f27fa4b3f0b14b524d0f86f0aee2",
+    "url": "/static/media/dummy-project.1976f27f.png"
+  },
+  {
+    "revision": "9aece68254c95eca4a0875e213e1d521",
+    "url": "/static/media/focused_working.9aece682.svg"
+  }
+]);
